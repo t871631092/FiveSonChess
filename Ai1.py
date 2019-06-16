bfe547a5749fd506a560baff49980b6830e55191
Zrxrxrx <Zrx23333@gmail.com>
2019-06-16 19:04:26 +1000

    tablescore error

--- Ai1.py
+++ Ai1.py
@@ -1,7 +1,7 @@
 import random
 import main
 
-ScoreModel=[(10, (0,1,1,0,0)), #Model of chest
+ScoreModel=[(10, (0,1,1,0,0)), #Model of chest 好像没什么卵用?
             (10, (0,0,1,1,0)),
             (20, (1,1,0,1,0)),
             (30, (0,0,1,1,1)),
@@ -16,14 +16,18 @@ ScoreModel=[(10, (0,1,1,0,0)), #Model of chest
             (100,(0,1,1,1,1,0)),
             (999,(1,1,1,1,1,1))]
 
-def scan()
+def scan():
     score = 0
-    global chest_table
-    for a in range(0,19):
-        for b in range(0,19):
-            if chest_table[a][b] == 0:
-                score += 0
-    break
+    #global chest_table
+    x=0
+    y=0
+    count=0
+    tablescore = [[[0 for count in range(5)] for x range(19)] for y in range(19)]
+    print(tablesocore)
+    # for a in range(0,19):
+    #     for b in range(0,19):
+    #         if chest_table[a][b] == 0:
+    #             score += 0
+    # break
 
 scan()
-
