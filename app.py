86954b1f5313fb97c458592444fca88f53f9a44c
kqq <971340511@qq.com>
2019-06-18 21:15:12 +0800

    add note

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-import ChessApp as app
+import ChessApp
 
-chess=app.PVP()
+#New a instance of ChessAPP
+#创建一个ChessAPP的实例
+
+#column can be set
+# Use 
+#chess=ChessApp.PVP(column=10)
+#default :column=20
+
+chess=ChessApp.PVP()
 
