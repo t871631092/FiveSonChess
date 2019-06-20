a44fcbb96295ca03a311642bb5d88fd6685526f2
xuyt <xuyt@shanghaitech.edu.cn>
2019-06-21 01:42:38 +0800

    fix the problem of the sanitytest due to the exe. path

--- Test/test_GUI.py
+++ Test/test_GUI.py
@@ -2,6 +2,16 @@
 @author: liubai01
 @description: this is the sanity test which trigger the GUI and have a contest with a random Agent
 """
+
+# fix the problem that cannot find moudule under root directory if you do not specify your
+# exe path there.
+import sys
+import os
+
+test_script_path = os.path.split(os.path.abspath(__file__))[0]
+root_path = os.path.split(test_script_path)[0]
+sys.path.append(root_path)
+
 import ChessApp
 import random
 from GUI.v1 import get_inputter
@@ -17,6 +27,8 @@ def AiZero(column, table, tableList, tableStr, lastChess, currentPlayer, p1, p2,
     print(str(round) + " : " + currentPlayerName + " chess on [ " + str(x) + "," + str(y) + " ]")
 
     return [x, y]
+
+
 
 gui_v1_input = get_inputter()
 chess = ChessApp.start(column=20, inp1=AiZero, inp2=gui_v1_input, p1Name="a1", p2Name="AiOne", autoPrint=False)
